78bf442b0a4063158ee84c5d1d2ff52f764abaac
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-20 15:18:05 +0200

    Add AppImages config for image asset paths

---
+++ frontend/lib/core/configs/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const String basePath = 'assets/images/';
+  static const String formatPNG = '.png';
+
+  static const String splashIcon = '${basePath}full-icon$formatPNG';
+}
