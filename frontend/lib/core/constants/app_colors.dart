c70ff7144f92bc8c4e346dcaa62231b6c2bd7f2d
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-20 15:18:35 +0200

    Add AppColors constants

---
+++ frontend/lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF10A35E);
+  static const lightGreen = Color(0xFF17C46D);
+  static const darkGreen = Color(0xFF0E3D27);
+  static const lightBackground = Color(0xffF2F2F2);
+  static const darkBackground = Color(0xff0D0C0C);
+}
