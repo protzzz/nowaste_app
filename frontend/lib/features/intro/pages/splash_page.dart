4295fdede0b284ea6185722b8fe6e91580ece0a5
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-29 03:08:53 +0100

    Update splash page animations and navigation

--- frontend/lib/features/intro/pages/splash_page.dart
+++ frontend/lib/features/intro/pages/splash_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:nowaste_app/core/configs/app_images.dart';
 import 'package:nowaste_app/core/constants/app_colors.dart';
 import 'package:nowaste_app/core/extensions/navx.dart';
-import 'package:nowaste_app/features/intro/pages/welcome.dart';
+import 'package:nowaste_app/features/auth/pages/signup_page.dart';
+import 'package:nowaste_app/features/intro/pages/welcome_page.dart';
 
 class SplashPage extends StatefulWidget {
   static const String routeName = '/splash-page';
@@ -18,6 +19,9 @@ class _SplashScreenState extends State<SplashPage>
   late Animation<Alignment> _alignment1;
   late Animation<Alignment> _alignment2;
   late Animation<Alignment> _alignment3;
+  late Animation<double> _radius1;
+  late Animation<double> _radius2;
+  late Animation<double> _radius3;
 
   @override
   void initState() {
@@ -25,29 +29,39 @@ class _SplashScreenState extends State<SplashPage>
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(microseconds: 100),
+      duration: Duration(seconds: 5),
     )..repeat(reverse: true);
 
-    _alignment1 = AlignmentTween(
-      begin: Alignment(-1.5, -1.2),
-      end: const Alignment(1.5, 1.2),
+    _alignment1 = Tween<Alignment>(
+      begin: Alignment.topLeft,
+      end: Alignment.centerLeft,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _alignment2 = Tween<Alignment>(
+      begin: Alignment.centerRight,
+      end: Alignment.topCenter,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _alignment3 = Tween<Alignment>(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
     ).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
 
-    _alignment2 = AlignmentTween(
-      begin: const Alignment(1.5, 1.2),
-      end: const Alignment(-1.5, -1.2),
-    ).animate(
+    _radius1 = Tween<double>(begin: 0.4, end: 0.6).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _radius2 = Tween<double>(begin: 0.3, end: 0.5).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+    _radius3 = Tween<double>(begin: 0.5, end: 1.0).animate(
       CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
     );
 
-    _alignment3 = AlignmentTween(
-      begin: const Alignment(0.0, -1.5),
-      end: const Alignment(0.0, 1.5),
-    ).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
+    redirect();
   }
 
   @override
@@ -59,76 +73,75 @@ class _SplashScreenState extends State<SplashPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.darkGreen,
       body: AnimatedBuilder(
         animation: _controller,
         builder: (context, child) {
           return Stack(
-            fit: StackFit.expand,
-
             children: [
-              Stack(
-                fit: StackFit.expand,
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(
-                      gradient: RadialGradient(
-                        center: Alignment(-0.8, -0.6),
-                        radius: 1.0,
-                        colors: [
-                          AppColors.primary,
-                          AppColors.darkGreen,
-                        ],
-                        stops: [0.3, 1.0],
-                      ),
-                    ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    center: _alignment1.value,
+                    radius: _radius1.value,
+                    colors: [
+                      Colors.orange.shade900,
+                      // AppColors.primary,
+                      Colors.transparent,
+                    ],
+                    stops: const [0.25, 1.0],
                   ),
-
-                  Container(
-                    decoration: const BoxDecoration(
-                      gradient: RadialGradient(
-                        center: Alignment(0.8, 0.6),
-                        radius: 0.8,
-                        colors: [
-                          AppColors.lightGreen,
-                          Colors.transparent,
-                        ],
-                        stops: [0.0, 1.0],
-                      ),
-                    ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    center: _alignment2.value,
+                    radius: _radius2.value,
+                    colors: [
+                      // AppColors.lightGreen,
+                      Colors.red.shade800,
+                      Colors.transparent,
+                    ],
+                    stops: const [0.0, 1.0],
                   ),
-
-                  Container(
-                    decoration: const BoxDecoration(
-                      gradient: RadialGradient(
-                        center: Alignment(0.0, -0.9),
-                        radius: 0.7,
-                        colors: [
-                          Color(0xFF23D976),
-                          Colors.transparent,
-                        ],
-                        stops: [0.0, 1.0],
-                      ),
-                    ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: RadialGradient(
+                    center: _alignment3.value,
+                    radius: _radius3.value,
+                    colors: const [
+                      Color(0xFF23D976),
+                      Colors.transparent,
+                    ],
+                    stops: const [0.0, 1.0],
                   ),
-                  Row(
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 200),
+                child: Center(
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(width: 100, AppImages.splashIcon),
                       Text(
                         'No Waste',
                         style: TextStyle(
-                          fontWeight: FontWeight.w600,
+                          fontWeight: FontWeight.w700,
                           fontSize: 50,
                           color: Colors.white,
                           // fontFamily: 'Roboto',
                           // fontFamily: 'Satoshi',
                           fontFamily: 'Nunito',
-                          letterSpacing: -1,
+                          wordSpacing: -10,
                         ),
                       ),
                     ],
                   ),
-                ],
+                ),
               ),
             ],
           );
@@ -140,6 +153,6 @@ class _SplashScreenState extends State<SplashPage>
   Future<void> redirect() async {
     await Future.delayed(Duration(seconds: 2));
     if (!mounted) return;
-    context.replaceWith(WelcomePage.routeName);
+    context.replaceWith(SignUpPage.routeName);
   }
 }
