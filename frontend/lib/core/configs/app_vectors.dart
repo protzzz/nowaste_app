2867b724b52535a351f1655e2eb7244f0a5e148c
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-29 03:03:34 +0100

    Add AppVectors config for SVG asset paths

---
+++ frontend/lib/core/configs/app_vectors.dart
@@ -0,0 +1,9 @@
+class AppVectors {
+  static const String basePath = 'assets/vectors/';
+  static const String format = '.svg';
+
+  static const String moon = '${basePath}moon$format';
+  static const String sun = '${basePath}sun$format';
+  static const String appleIcon = '${basePath}apple-icon$format';
+  static const String googleIcon = '${basePath}google-icon$format';
+}
