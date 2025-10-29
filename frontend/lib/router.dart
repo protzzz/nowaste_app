8b1db62af5130417689665656352e5bfbdaef370
protzzz <99717913+protzzz@users.noreply.github.com>
2025-10-29 03:09:50 +0100

    Add routes for login, signup, intro, and home pages

--- frontend/lib/router.dart
+++ frontend/lib/router.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:nowaste_app/features/auth/pages/login_page.dart';
+import 'package:nowaste_app/features/auth/pages/signup_page.dart';
+import 'package:nowaste_app/features/home/pages/home_page.dart';
+import 'package:nowaste_app/features/intro/pages/intro_page.dart';
 import 'package:nowaste_app/features/intro/pages/splash_page.dart';
-import 'package:nowaste_app/features/intro/pages/welcome.dart';
+import 'package:nowaste_app/features/intro/pages/welcome_page.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
+    case LoginPage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => LoginPage(),
+      );
+    case SignUpPage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => SignUpPage(),
+      );
     case WelcomePage.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => WelcomePage(),
       );
+    case IntroPage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => IntroPage(),
+      );
     case SplashPage.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => SplashPage(),
+      );
+    case HomePage.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => HomePage(),
       );
     default:
       return MaterialPageRoute(
