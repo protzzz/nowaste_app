4aa2b6d046ae211ff2f527ec0758b2d4e1934e84
protzzz <99717913+protzzz@users.noreply.github.com>
2025-11-25 11:44:28 +0100

    Add AuthCubit integration

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nowaste_app/core/configs/app_theme.dart';
+import 'package:nowaste_app/features/auth/cubit/auth_cubit.dart';
+import 'package:nowaste_app/features/home/pages/home_page.dart';
 import 'package:nowaste_app/features/intro/pages/splash_page.dart';
 import 'package:nowaste_app/router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => AuthCubit())],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthCubit>().getUserData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +38,14 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       home: SplashPage(),
+      // home: BlocBuilder<AuthCubit, AuthState>(
+      //   builder: (context, state) {
+      //     if (state is AuthLoggedIn) {
+      //       return HomePage();
+      //     }
+      //     return SplashPage();
+      //   },
+      // ),
     );
   }
 }
