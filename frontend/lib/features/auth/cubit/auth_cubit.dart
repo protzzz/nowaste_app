6ee255e3e6c95e02742e0ce4e67865a5ae8440b5
protzzz <99717913+protzzz@users.noreply.github.com>
2025-11-27 05:34:49 +0100

    Add AuthCubit and AuthState for authentication

---
+++ frontend/lib/features/auth/cubit/auth_cubit.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nowaste_app/core/services/shared_preferences_service.dart';
+import 'package:nowaste_app/features/auth/repository/auth_remote_repository.dart';
+import 'package:nowaste_app/models/user_model.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+  final authRemoteRepository = AuthRemoteRepository();
+  final sharedPreferencesService = SharedPreferencesService();
+
+  void getUserData() async {
+    try {
+      emit(AuthLoading());
+
+      final userModel = await authRemoteRepository.getUserData();
+      // print(userModel);
+
+      if (userModel != null) {
+        emit(AuthLoggedIn(userModel));
+      } else {
+        emit(AuthInitial());
+      }
+    } catch (error) {
+      emit(AuthError(error.toString()));
+    }
+  }
+
+  // Future<UserModel?> getUserData() async {
+  //   try {
+  //     emit(AuthLoading());
+
+  //     final userModel = await authRemoteRepository.getUserData();
+  //     print(userModel);
+
+  //     if (userModel == null) {
+  //       emit(AuthInitial());
+  //       return null;
+  //     } else {
+  //       emit(AuthLoggedIn(userModel));
+  //       return userModel;
+  //     }
+  //   } catch (e) {
+  //     emit(AuthError(e.toString()));
+  //     return null;
+  //   }
+  // }
+
+  void signUp({
+    required String name,
+    required String surname,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+
+      final user = await authRemoteRepository.signUp(
+        name: name,
+        surname: surname,
+        email: email,
+        password: password,
+      );
+
+      emit(AuthSignUp());
+      // print(userModel);
+    } catch (error) {
+      emit(AuthError(error.toString()));
+    }
+  }
+
+  void login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+
+      final user = await authRemoteRepository.login(
+        email: email,
+        password: password,
+      );
+
+      if (user.token.isNotEmpty) {
+        await sharedPreferencesService.setToken(user.token);
+      }
+
+      emit(AuthLoggedIn(user));
+    } catch (error) {
+      emit(AuthError(error.toString()));
+    }
+  }
+}
