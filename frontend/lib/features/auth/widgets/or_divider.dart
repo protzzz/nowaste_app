d978c74a8a14ab5fa351f39fc0a2d793470b5ed8
protzzz <99717913+protzzz@users.noreply.github.com>
2025-11-27 05:33:48 +0100

    Update import path

--- frontend/lib/features/auth/widgets/or_divider.dart
+++ frontend/lib/features/auth/widgets/or_divider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nowaste_app/core/constants/app_colors.dart';
+import 'package:nowaste_app/core/configs/app_colors.dart';
 
 class OrDivider extends StatelessWidget {
   const OrDivider({super.key});
