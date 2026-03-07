244bdf8d871a8d5cf93f7bddd910d965c28a1902
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-07 01:14:08 +0100

    Add additional vector asset constants

--- frontend/lib/core/configs/app_vectors.dart
+++ frontend/lib/core/configs/app_vectors.dart
@@ -6,4 +6,24 @@ class AppVectors {
   static const String sun = '${basePath}sun$format';
   static const String appleIcon = '${basePath}apple-icon$format';
   static const String googleIcon = '${basePath}google-icon$format';
+
+  static const String fridgeIcon = '${basePath}fridge-icon$format';
+  static const String fridgeFilledIcon =
+      '${basePath}fridge-filled-icon$format';
+
+  static const String recipesIcon = '${basePath}recipes-icon$format';
+  static const String recipesIcon2 =
+      '${basePath}recipes-icon-2$format';
+  static const String recipesFilledIcon =
+      '${basePath}recipes-filled-icon$format';
+
+  static const String notificationIcon =
+      '${basePath}notification-icon$format';
+  static const String notificationFilledIcon =
+      '${basePath}notification-filled-icon$format';
+
+  static const String settingsIcon =
+      '${basePath}settings-icon$format';
+  static const String settingsFilledIcon =
+      '${basePath}settings-filled-icon$format';
 }
