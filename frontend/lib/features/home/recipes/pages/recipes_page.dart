a9ea0370d2335d43088b4b09cf6a75f9de19522a
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-07 16:46:15 +0100

    Add RecipesPage, SettingsPage, and ShoppingPage widgets for navigation

---
+++ frontend/lib/features/home/recipes/pages/recipes_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipesPage extends StatelessWidget {
+  const RecipesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Recipes Page'));
+  }
+}
