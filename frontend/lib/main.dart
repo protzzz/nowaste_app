7c8240a5cbedb39d6cb30df0eb58a9761f933923
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-07 16:46:38 +0100

    Add NavigationCubit to MultiBlocProvider for improved navigation management

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nowaste_app/core/configs/app_theme.dart';
 import 'package:nowaste_app/features/auth/cubit/auth_cubit.dart';
+import 'package:nowaste_app/features/home/navigation/cubit/navigation_cubit.dart';
 import 'package:nowaste_app/features/intro/pages/splash_page.dart';
 import 'package:nowaste_app/router.dart';
 
 void main() {
   runApp(
     MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => AuthCubit())],
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+        BlocProvider(create: (_) => NavigationCubit()),
+      ],
       child: const MyApp(),
     ),
   );
