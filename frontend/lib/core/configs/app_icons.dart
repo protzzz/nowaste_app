ef3dafd509912819644f0ba6ad030e1853f1cb7f
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-10 20:55:25 +0100

    Remove images folder, store everythins in assets/icons

---
+++ frontend/lib/core/configs/app_icons.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String basePath = 'assets/icons/';
+  static const String formatPNG = '.png';
+
+  static const String splashIcon = '${basePath}full-icon$formatPNG';
+  static const String chef = '${basePath}chef$formatPNG';
+  static const String cooking = '${basePath}cooking$formatPNG';
+  static const String setting = '${basePath}setting$formatPNG';
+}
--- frontend/lib/core/configs/app_images.dart
+++
@@ -1,6 +0,0 @@
-class AppImages {
-  static const String basePath = 'assets/images/';
-  static const String formatPNG = '.png';
-
-  static const String splashIcon = '${basePath}full-icon$formatPNG';
-}
