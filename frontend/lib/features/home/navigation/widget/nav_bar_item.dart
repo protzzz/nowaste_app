fcb54d1e7eb7789d1edbb8a9da3fea491b5fb6d6
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-14 16:41:14 +0100

    Update NavBarItem logic

--- frontend/lib/features/home/navigation/widget/nav_bar_item.dart
+++ frontend/lib/features/home/navigation/widget/nav_bar_item.dart
@@ -1,55 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:nowaste_app/core/configs/app_colors.dart';
 
 class NavBarItem extends StatelessWidget {
   final String label;
-  final bool isSelected;
-  final String selectedSvgPath;
-  final String unselectedSvgPath;
-  final double widthSelected;
-  final double widthUnselected;
+  final String iconPath;
   final VoidCallback onTap;
+  final bool isSelected;
+  final double width;
+
+  final Color color;
 
   const NavBarItem({
     super.key,
     required this.label,
-    required this.isSelected,
-    required this.selectedSvgPath,
-    required this.unselectedSvgPath,
-    required this.widthSelected,
-    required this.widthUnselected,
+    required this.iconPath,
     required this.onTap,
+    required this.isSelected,
+    required this.width,
+    required this.color,
   });
+
+  bool get _isSvg => iconPath.toLowerCase().endsWith('.svg');
 
   @override
   Widget build(BuildContext context) {
-    final selectedColor = AppColors.lightGreen;
-    final unselectedColor = Colors.grey.shade600;
-
-    final color = isSelected ? selectedColor : unselectedColor;
-    final iconPath =
-        isSelected ? selectedSvgPath : unselectedSvgPath;
-    final width = isSelected ? widthSelected : widthUnselected;
-
-    return InkWell(
-      onTap: onTap,
-      borderRadius: BorderRadius.circular(130),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SvgPicture.asset(iconPath, width: width, color: color),
-          const SizedBox(height: 3),
-          Text(
-            label,
-            style: TextStyle(
-              color: color,
-              fontSize: 12,
-              fontWeight: FontWeight.w500,
-            ),
+    return Expanded(
+      child: GestureDetector(
+        onTap: onTap,
+        behavior: HitTestBehavior.opaque,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeOut,
+          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+          decoration: BoxDecoration(
+            color:
+                isSelected
+                    // ? Color(0xFFF2F2F7)
+                    ? const Color.fromARGB(239, 238, 238, 238)
+                    : Colors.transparent,
+            borderRadius: BorderRadius.circular(40),
           ),
-        ],
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildIcon(),
+              const SizedBox(height: 3),
+              Text(
+                label,
+                style: TextStyle(fontSize: 11, fontWeight: FontWeight.w500, color: color),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget _buildIcon() {
+    if (_isSvg) {
+      return SvgPicture.asset(
+        iconPath,
+        width: width,
+        height: width,
+        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      );
+    }
+
+    return Image.asset(iconPath, width: width, height: width, color: color);
   }
 }
