1413ee2131a5663d0070421c8569de47d7dde732
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-14 16:40:29 +0100

    Update icon paths

--- frontend/lib/core/configs/app_icons.dart
+++ frontend/lib/core/configs/app_icons.dart
@@ -1,9 +1,18 @@
-class AppImages {
-  static const String basePath = 'assets/icons/';
+class AppIcons {
+  static const String basePath = 'assets/icons';
   static const String formatPNG = '.png';
+  static const String formatJPG = '.jpg';
 
-  static const String splashIcon = '${basePath}full-icon$formatPNG';
-  static const String chef = '${basePath}chef$formatPNG';
-  static const String cooking = '${basePath}cooking$formatPNG';
-  static const String setting = '${basePath}setting$formatPNG';
+  // Splash Screen
+  static const String splashIcon = 'assets/icons/main/full-icon.png';
+
+  // Bottom Navigation Bar
+  static const String alert = '$basePath/navigation-bar/alert$formatPNG';
+  static const String alertFilled = '$basePath/navigation-bar/alert-filled$formatPNG';
+  static const String chef = '$basePath/navigation-bar/chef$formatPNG';
+  static const String chefFilled = '$basePath/navigation-bar/chef-filled$formatPNG';
+  static const String cooking = '$basePath/navigation-bar/cooking$formatPNG';
+  static const String cookingFilled = '$basePath/navigation-bar/cooking-filled$formatPNG';
+  static const String settings = '$basePath/navigation-bar/settings$formatPNG';
+  static const String settingsFilled = '$basePath/navigation-bar/settings-filled$formatPNG';
 }
