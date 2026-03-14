280b4d995dbea066766add74af2f10b7937b9bdc
protzzz <99717913+protzzz@users.noreply.github.com>
2026-03-14 16:42:38 +0100

    Update NavBar implementation and improved UI, success version with no FAB button

--- frontend/lib/features/home/navigation/widget/app_bottom_bar.dart
+++ frontend/lib/features/home/navigation/widget/app_bottom_bar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nowaste_app/core/configs/app_colors.dart';
+import 'package:nowaste_app/core/configs/app_icons.dart';
 import 'package:nowaste_app/core/configs/app_vectors.dart';
 import 'package:nowaste_app/features/home/navigation/cubit/navigation_cubit.dart';
 import 'package:nowaste_app/features/home/navigation/widget/nav_bar_item.dart';
@@ -13,6 +15,11 @@ class AppBottomBar extends StatelessWidget {
       builder: (context, state) {
         final bottomInset = MediaQuery.of(context).padding.bottom;
 
+        final selectedMainColor = AppColors.lightGreen;
+        final inactiveColor = AppColors.darkBackground;
+        // final selectedBackgroundColor = selectedMainColor.withValues(alpha: 0.12);
+        // final selectedBorderColor = selectedMainColor.withValues(alpha: 0.18);
+
         return SafeArea(
           top: false,
           child: Padding(
@@ -20,139 +27,122 @@ class AppBottomBar extends StatelessWidget {
               12,
               0,
               12,
-              bottomInset > 0 ? 8 : 12,
+              bottomInset > 0 ? 3 : 6,
+              // 0,
             ),
             child: Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Expanded(
                   child: Container(
-                    height: 74,
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 8,
-                      vertical: 6,
-                    ),
+                    // height: 62,
+                    padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 4),
                     decoration: BoxDecoration(
-                      color: Colors.white.withValues(alpha: 0.94),
-                      borderRadius: BorderRadius.circular(32),
+                      color: Colors.white.withValues(alpha: 0.88),
+                      borderRadius: BorderRadius.circular(50),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black.withValues(
-                            alpha: 0.08,
-                          ),
-                          blurRadius: 24,
-                          offset: const Offset(0, 8),
+                          color: Colors.black.withValues(alpha: 0.08),
+                          blurRadius: 18,
+                          offset: const Offset(0, 6),
                         ),
                       ],
                     ),
                     child: Row(
-                      mainAxisAlignment:
-                          MainAxisAlignment.spaceAround,
                       children: [
-                        Expanded(
-                          child: NavBarItem(
-                            label: 'Fridge',
-                            isSelected:
-                                state == NavigationItem.fridge,
-                            selectedSvgPath:
-                                AppVectors.fridgeFilledIcon,
-                            unselectedSvgPath: AppVectors.fridgeIcon,
-                            widthSelected: 27,
-                            widthUnselected: 27,
-                            onTap:
-                                () => context
-                                    .read<NavigationCubit>()
-                                    .setPage(NavigationItem.fridge),
-                          ),
+                        // Fridge Nav Bar Item
+                        NavBarItem(
+                          label: 'Fridge',
+
+                          isSelected: state == NavigationItem.fridge,
+                          // iconPath: AppVectors.fridgeIcon,
+                          iconPath: AppVectors.fridgeFilledIcon,
+                          width: 24,
+                          color: state == NavigationItem.fridge ? selectedMainColor : inactiveColor,
+
+                          onTap:
+                              () => context.read<NavigationCubit>().setPage(NavigationItem.fridge),
                         ),
-                        Expanded(
-                          child: NavBarItem(
-                            label: 'Recipes',
-                            isSelected:
-                                state == NavigationItem.recipes,
-                            selectedSvgPath:
-                                AppVectors.recipesFilledIcon,
-                            unselectedSvgPath:
-                                AppVectors.recipesIcon2,
-                            widthSelected: 28,
-                            widthUnselected: 28,
-                            onTap:
-                                () => context
-                                    .read<NavigationCubit>()
-                                    .setPage(NavigationItem.recipes),
-                          ),
+
+                        // Recipes Nav Bar Item
+                        NavBarItem(
+                          label: 'Recipes',
+
+                          isSelected: state == NavigationItem.recipes,
+                          // iconPath: AppIcons.cooking,
+                          iconPath: AppIcons.cookingFilled,
+                          // iconPath: AppIcons.chef,
+                          width: 24,
+                          color:
+                              state == NavigationItem.recipes ? selectedMainColor : inactiveColor,
+                          onTap:
+                              () => context.read<NavigationCubit>().setPage(NavigationItem.recipes),
                         ),
-                        Expanded(
-                          child: NavBarItem(
-                            label: 'Alerts',
-                            isSelected:
-                                state ==
+
+                        // Alerts Nav Bar Item
+                        NavBarItem(
+                          label: 'Alerts',
+
+                          isSelected: state == NavigationItem.notifications,
+                          // iconPath: AppIcons.alert,
+                          iconPath: AppIcons.alertFilled,
+                          width: 24,
+                          color:
+                              state == NavigationItem.notifications
+                                  ? selectedMainColor
+                                  : inactiveColor,
+                          onTap:
+                              () => context.read<NavigationCubit>().setPage(
                                 NavigationItem.notifications,
-                            selectedSvgPath:
-                                AppVectors.notificationFilledIcon,
-                            unselectedSvgPath:
-                                AppVectors.notificationIcon,
-                            widthSelected: 26,
-                            widthUnselected: 25,
-                            onTap:
-                                () => context
-                                    .read<NavigationCubit>()
-                                    .setPage(
-                                      NavigationItem.notifications,
-                                    ),
-                          ),
+                              ),
                         ),
-                        Expanded(
-                          child: NavBarItem(
-                            label: 'Settings',
-                            isSelected:
-                                state == NavigationItem.settings,
-                            selectedSvgPath:
-                                AppVectors.settingsFilledIcon,
-                            unselectedSvgPath:
-                                AppVectors.settingsIcon,
-                            widthSelected: 26,
-                            widthUnselected: 25,
-                            onTap:
-                                () => context
-                                    .read<NavigationCubit>()
-                                    .setPage(
-                                      NavigationItem.settings,
-                                    ),
-                          ),
+
+                        // Settings Nav Bar Item
+                        NavBarItem(
+                          label: 'Settings',
+
+                          isSelected: state == NavigationItem.settings,
+                          // iconPath: AppIcons.settings,
+                          iconPath: AppIcons.settingsFilled,
+                          width: 24,
+                          color:
+                              state == NavigationItem.settings ? selectedMainColor : inactiveColor,
+                          onTap:
+                              () =>
+                                  context.read<NavigationCubit>().setPage(NavigationItem.settings),
                         ),
                       ],
                     ),
                   ),
                 ),
-                const SizedBox(width: 12),
-                GestureDetector(
-                  onTap: () {},
-                  child: Container(
-                    width: 74,
-                    height: 74,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withValues(alpha: 0.94),
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withValues(
-                            alpha: 0.08,
-                          ),
-                          blurRadius: 24,
-                          offset: const Offset(0, 8),
-                        ),
-                      ],
-                    ),
-                    child: const Center(
-                      child: Icon(
-                        Icons.add,
-                        size: 30,
-                        color: Colors.black87,
-                      ),
-                    ),
-                  ),
-                ),
+                // const SizedBox(width: 12),
+
+                // GestureDetector(
+                //   onTap: () {},
+                //   child: Container(
+                //     width: 60,
+                //     height: 74,
+                //     decoration: BoxDecoration(
+                //       color: Colors.white.withValues(alpha: 0.94),
+                //       shape: BoxShape.circle,
+                //       boxShadow: [
+                //         BoxShadow(
+                //           color: Colors.black.withValues(
+                //             alpha: 0.08,
+                //           ),
+                //           blurRadius: 24,
+                //           offset: const Offset(0, 8),
+                //         ),
+                //       ],
+                //     ),
+                //     child: const Center(
+                //       child: Icon(
+                //         Icons.add,
+                //         size: 30,
+                //         color: Colors.black87,
+                //       ),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
